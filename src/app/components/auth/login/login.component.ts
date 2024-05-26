947a0c750703767c45dd118cdd95577d03e5addb
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-05-25 23:57:43 -0500

    first commit~

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { LoginService } from "../services/login.service";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: LoginService) {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log('Username:', this.loginForm.value.username);
+    console.log('Password:', this.loginForm.value.password);
+  }
+
+  // ngOnInit() {
+  //   this.loginForm = this.formBuilder.group({
+  //     username: ['', Validators.required],
+  //     password: ['', Validators.required]
+  //   });
+  // }
+
+  // onSubmit() {
+  //   if (this.loginForm.valid) {
+  //     const {username, password} = this.loginForm.value;
+  //     this.authService.login(username, password).subscribe(
+  //         response => {
+  //           // Aquí puedes manejar la respuesta de la API
+  //           console.log(response);
+  //         },
+  //         error => {
+  //           // Aquí puedes manejar los errores
+  //           console.error(error);
+  //         }
+  //     );
+  //   }
+  // }
+}
